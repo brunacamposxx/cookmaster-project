04291c83714ac8d75acee30a3d557f13b40a9255
Bruna Campos <92brunacampos@gmail.com>
2021-11-20 20:23:14 -0300

    style(eslint): fix ESLint

--- src/middlewares/validation.js
+++ src/middlewares/validation.js
@@ -51,7 +51,7 @@ const missingAuthToken = (req, res, next) => {
   const token = req.headers.authorization;
 
   if (!token) {
-    return res.status(401).json({ message: 'missing auth token'});
+    return res.status(401).json({ message: 'missing auth token' });
   }
   next();
 };
